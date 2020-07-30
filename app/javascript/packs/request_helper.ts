74997a1be057136de5898925fb4e33717a1dd2a4
Marek Waligórski <marek@brainsandbeards.com>
2020-07-31 00:15:59 +0200

    Add a request helper

---
+++ app/javascript/packs/request_helper.ts
@@ -0,0 +1,8 @@
+const makeRequest = (url, method, data) => {
+  return fetch(url, {
+    body: JSON.stringify(data),
+    headers: { "Content-type": "application/json" },
+  });
+};
+
+// makeRequest('http://localhost:3000/todos', "POST", {todo: {name: 'dupa', done: false}}).then(res => res.json()).then(data => console.log(data))
