c768c4861e72e58c668590d2e0e7d4b3ac919a01
Marek Waligórski <marek@brainsandbeards.com>
2020-08-04 16:23:35 +0200

    Add example for using refs

---
+++ app/javascript/packs/test_refs.jsx
@@ -0,0 +1,26 @@
+class CustomTextInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+    this.focusTextInput = this.focusTextInput.bind(this);
+  }
+
+  focusTextInput() {
+    // Note: we need to access "current" to get the DOM node
+    this.textInput.current.focus();
+  }
+
+  render() {
+    // Associate the <input> with the `textInput` ref
+    return (
+      <div>
+        <input type="text" ref={this.textInput} />
+        <input
+          type="button"
+          value="Focus the text input"
+          onClick={this.focusTextInput}
+        />
+      </div>
+    );
+  }
+}
